a7500897033e585b3ccfd2f900c143aabd5f416f
Dennis Kennetz <drkennetz@gmail.com>
2024-05-05 12:38:25 -0500

    Adding first hello world example.

---
+++ 001_setup_check/hello_world.cu
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+
+// Device (GPU) code
+__global__ void helloFromGPU()
+{
+    int threadId = blockIdx.x * blockDim.x + threadIdx.x;
+    printf("Hello from GPU thread: %d!\n", threadId);
+}
+
+// Host code
+int main()
+{
+  int nDevices;
+  cudaGetDeviceCount(&nDevices);
+  for (int i = 0; i < nDevices; i++) {
+    cudaDeviceProp prop;
+    cudaGetDeviceProperties(&prop, i);
+    printf("Device Number: %d\n", i);
+    printf("  Device name: %s\n", prop.name);
+    printf("  Memory Clock Rate (MHz): %d\n",
+           prop.memoryClockRate/1024);
+    printf("  Memory Bus Width (bits): %d\n",
+           prop.memoryBusWidth);
+    printf("  Peak Memory Bandwidth (GB/s): %.1f\n",
+           2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6);
+    printf("  Total global memory (Gbytes) %.1f\n",(float)(prop.totalGlobalMem)/1024.0/1024.0/1024.0);
+    printf("  Shared memory per block (Kbytes) %.1f\n",(float)(prop.sharedMemPerBlock)/1024.0);
+    printf("  minor-major: %d-%d\n", prop.minor, prop.major);
+    printf("  Warp-size: %d\n", prop.warpSize);
+    printf("  Concurrent kernels: %s\n", prop.concurrentKernels ? "yes" : "no");
+    printf("  Concurrent computation/communication: %s\n\n",prop.deviceOverlap ? "yes" : "no");
+  }
+  printf("\n");
+  printf("Hello from CPU!\n");
+  helloFromGPU<<<1,32>>>();
+  cudaDeviceSynchronize();
+  return 0;
+}
+
